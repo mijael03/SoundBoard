7b71d292ef23f4051563e360438dbf4c9896e578
Mijael <mijael.cama@tecsup.edu.pe>
2022-05-20 15:43:15 -0500

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Mijael Cama on 20/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
